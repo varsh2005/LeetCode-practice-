81a8122bbd5a0afeeb8894e224684c963559288e
varsh2005 <119554047+varsh2005@users.noreply.github.com>
2025-06-21 20:12:02 +0530

    Create ClimbingStairs.java

---
+++ ClimbingStairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=1) return 1;
+        if(n<=2) return 2;
+        int first=1, second=2;
+        for(int i=3;i<=n;i++){
+            int temp = second;
+            second = first+ second;
+            first = temp;
+        }
+        return second;
+    }
+}
